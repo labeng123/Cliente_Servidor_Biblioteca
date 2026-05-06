7b3fa57ae64aaa1ebefa1fba2f1ebf59401abae8
Artur Siqueira <artur.sudre@gmail.com>
2026-05-06 11:22:21 -0300

    Commit Ajustando

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,10 @@
 from flask import Flask
-from flask_cors import CORS # <-- NOVA IMPORTAÇÃO
+from flask_cors import CORS 
 
-# ... suas importações de infraestrutura e aplicação ...
 from apresentacao.controladores.controlador import criar_blueprint_biblioteca
 
 def create_app():
     app = Flask(__name__)
-    CORS(app) # <-- LIBERANDO A COMUNICAÇÃO DE REDE
-
-    # O app.secret_key não é mais tão vital para Flash Messages, 
-    # mas pode manter para sessões futuras.
+    CORS(app) 
     app.secret_key = 'chave_secreta_mvp' 
 
-    # ... resto da sua injeção de dependência e registro do blueprint ...
